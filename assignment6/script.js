418d94cfd8d6e90418885397f16ebb219714f621
05simran <65066848+05simran@users.noreply.github.com>
2021-09-22 12:40:49 +0530

    dropdown to text
    this is a simple js file where the entered data is stored in an select option then further copied to a text box

---
+++ assignment6/script.js
@@ -0,0 +1,23 @@
+var arr = ['india', 'canada', 'austria'];
+// function addArray(){
+//     var inputVal = document.getElementById('inp').value;
+//     if( inputVal != 0){
+//         arr.push(inputVal);
+//         document.getElementById('showResult').value = '';
+//     }
+  
+// }
+// function showArray(){
+//     var len = arr.length;
+
+//     document.getElementById('showResult').innerHTML = '';
+//     if(len > 0){
+      
+// 				for(i=0; i<arr.length;i++){
+// 					document.getElementById('showResult').innerHTML += arr[i]+"<br>";
+// 				}
+// 			}else{
+// 				alert("No Array Values, Please add using the above text box");
+// 			}
+// 			}
+        
